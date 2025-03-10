125020a4402be8fd7e4ba771dab525962834e8d5
maggul <maggul@smu.edu>
2025-03-10 10:26:36 -0500

    3 versions of WRMS norm

--- gkeyll_diffusion/gk_diffusion_1x1v_p1.c
+++ gkeyll_diffusion/gk_diffusion_1x1v_p1.c
@@ -423,7 +423,7 @@ create_diffusion_ctx(void)
     .temp = 2.75, // Temperature.
     .mass = 1.0, // Species mass.
     .B0 = 1.0, // Magnetic field.
-    .diffD0 = 10.0, // Diffusion amplitude.
+    .diffD0 = 0.1, // Diffusion amplitude.
 
     .x_min = -M_PI, // Minimum x of the grid.
     .x_max =  M_PI, // Maximum x of the grid.
@@ -926,13 +926,13 @@ static int check_flag(void* flagvalue, const char* funcname, int opt)
 
 int flag;                /* reusable error-checking flag */
 
+sunrealtype reltol = SUN_RCONST(1.0e-5); /* tolerances */
+sunrealtype abstol = SUN_RCONST(1.0e-8);
+
 // Error weight function for global norm of y_{n-1}
 int efun_glob_norm(N_Vector x, N_Vector w, void *user_data) {
 
   sunrealtype xnorm;
-
-  sunrealtype reltol = SUN_RCONST(1.0e-5); /* tolerances */
-  sunrealtype abstol = SUN_RCONST(1.0e-8);
 
   struct gkyl_array* xdptr = NV_CONTENT_GKZ(x)->dataptr;
 
@@ -955,9 +955,6 @@ int efun_glob_norm(N_Vector x, N_Vector w, void *user_data) {
 int efun_cell_norm(N_Vector x, N_Vector w, void *user_data) {
 
   sunrealtype xcnorm;
-
-  sunrealtype reltol = SUN_RCONST(1.0e-5); /* tolerances */
-  sunrealtype abstol = SUN_RCONST(1.0e-8);
 
   struct gkyl_array* xdptr = NV_CONTENT_GKZ(x)->dataptr;
   struct gkyl_array* wdptr = NV_CONTENT_GKZ(w)->dataptr;
@@ -982,9 +979,6 @@ int efun_cell_norm(N_Vector x, N_Vector w, void *user_data) {
 /* general problem parameters */
 sunrealtype T0    = 0.0;  /* initial time */
 
-sunrealtype reltol = SUN_RCONST(1.0e-5); /* tolerances */
-sunrealtype abstol = SUN_RCONST(1.0e-8);
-
 int STS_init(struct gkyl_diffusion_app* app, N_Vector* y, void** arkode_mem)
 {
   /* Create the SUNDIALS context object for this simulation */
@@ -993,9 +987,10 @@ int STS_init(struct gkyl_diffusion_app* app, N_Vector* y, void** arkode_mem)
   if (check_flag(&flag, "SUNContext_Create", 1)) { return 1; }
 
   /* Initialize data structures */
-  *y = N_VMake_Gkylzero(app->f, app->use_gpu, sunctx);
-  if (check_flag((void*)*y, "N_VMake_Gkylzero", 0)) { return 1; }
-
+  if(*y == NULL) {
+    *y = N_VMake_Gkylzero(app->f, app->use_gpu, sunctx);
+    if (check_flag((void*)*y, "N_VMake_Gkylzero", 0)) { return 1; }
+  }
   /* Call LSRKStepCreateSTS to initialize the ARK timestepper module and
      specify the right-hand side function in y'=f(t,y), the initial time
      T0, and the initial dependent variable vector y. */
@@ -1051,9 +1046,10 @@ int SSP_init(struct gkyl_diffusion_app* app, N_Vector* y, void** arkode_mem, voi
   if (check_flag(&flag, "SUNContext_Create", 1)) { return 1; }
 
   /* Initialize data structures */
-  *y = N_VMake_Gkylzero(app->f, app->use_gpu, sunctx);
-  if (check_flag((void*)*y, "N_VMake_Gkylzero", 0)) { return 1; }
-
+  if(*y == NULL) {
+    *y = N_VMake_Gkylzero(app->f, app->use_gpu, sunctx);
+    if (check_flag((void*)*y, "N_VMake_Gkylzero", 0)) { return 1; }
+  }
   /* Call LSRKStepCreateSTS to initialize the ARK timestepper module and
      specify the right-hand side function in y'=f(t,y), the initial time
      T0, and the initial dependent variable vector y. */
@@ -1393,10 +1389,26 @@ write_data(struct gkyl_tm_trigger* iot, struct gkyl_diffusion_app* app, double t
   }
 }
 
+void compute_max_error(N_Vector u, N_Vector v, struct gkyl_diffusion_app* app) {
+  double error = 0;
+
+  struct gkyl_array* udptr = NV_CONTENT_GKZ(u)->dataptr;
+  struct gkyl_array* vdptr = NV_CONTENT_GKZ(v)->dataptr;
+
+  sunrealtype *u_data = udptr->data;
+  sunrealtype *v_data = vdptr->data;
+
+  sunindextype N = (udptr->size*udptr->ncomp);
+
+  for (int i = 0; i < N; i++) {
+    error = fmax(error, fabs(u_data[i] - v_data[i]));
+  }
+
+  printf("\nerror = %e\n", error);
+}
+
 int main(int argc, char **argv)
 {
-  clock_t begin = clock();
-
   bool is_STS = false;
   bool is_SSP = true;
   bool test_nvector = false;
@@ -1404,6 +1416,8 @@ int main(int argc, char **argv)
     test_nvector_gkylzero(false);
 
   int wrms_norm_type = 1;
+
+  bool compute_error = true;
 
   struct gkyl_app_args app_args = parse_app_args(argc, argv);
 
@@ -1464,7 +1478,54 @@ int main(int argc, char **argv)
 
   void* arkode_mem = NULL; /* empty ARKode memory structure */
   N_Vector y       = NULL; /* empty vector for storing solution */
+  N_Vector yref    = NULL; /* empty vector for storing solution */
 
+  struct gkyl_array *fref = mkarr(false, app->basis.num_basis, app->local_ext.volume);
+  gkyl_array_set(fref, 1.0, app->f);
+
+  clock_t begin = clock();
+  // compute the reference solution and the error
+  if(compute_error) {
+    /* Create the SUNDIALS context object for this simulation */
+    SUNContext sunctx;
+    flag = SUNContext_Create(SUN_COMM_NULL, &sunctx);
+    if (check_flag(&flag, "SUNContext_Create", 1)) { return 1; }
+
+    yref = N_VMake_Gkylzero(fref, app->use_gpu, sunctx);
+
+    double dt = 0.0;
+    double tout = 0;
+    double t_curr = 0.0;
+    int flag;
+
+    void* arkode_mem_ref = NULL;
+    flag = SSP_init(app, &yref, &arkode_mem_ref, &ctx);
+    if (check_flag(&flag, "SSP_init", 1)) { return 1; }
+
+    /* Specify the fixed step size */
+    flag = ARKodeSetFixedStep(arkode_mem_ref, 1.0e-05);
+    if (check_flag(&flag, "ARKodeSetFixedStep", 1)) { return 1; }
+
+    long int step = 1;
+    while ((t_end - t_curr > 1.0e-10) && (step <= app_args.num_steps)) {
+
+      if(step == 1) {
+      dt = 0.1;
+      flag = ARKodeGetCurrentTime(arkode_mem_ref, &t_curr);
+      if (check_flag(&flag, "ARKodeGetCurrentTime", 1)) { return 1; }
+      }
+      tout += dt;
+
+      gkyl_diffusion_update_STS(app, arkode_mem_ref, tout, yref, &t_curr);
+
+      step += 1;
+    }
+  }
+
+  clock_t end = clock();
+  double cpu_time_for_ref = (double)(end - begin) / CLOCKS_PER_SEC;
+
+  begin = clock();
   if (is_STS) {
     int flag;
     flag = STS_init(app, &y, &arkode_mem);
@@ -1567,12 +1628,18 @@ int main(int argc, char **argv)
     ARKodePrintAllStats(arkode_mem, stdout, SUN_OUTPUTFORMAT_TABLE);
   }
 
+  end = clock();
+  double cpu_time_used = (double)(end - begin) / CLOCKS_PER_SEC;
+
+  printf("\ncomputation time for the reference solution is %10.5f\n", cpu_time_for_ref);
+  printf("\ncomputation time is %10.5f\n", cpu_time_used);
+
+  if(compute_error) {
+    compute_max_error(y, yref, app);
+  }
+
   // Free the app.
   gkyl_diffusion_app_release(app);
 
-  clock_t end = clock();
-  double cpu_time_used = (double)(end - begin) / CLOCKS_PER_SEC;
-
-  printf("\ncomputation time is %10.5f\n", cpu_time_used);
-
+  return 0;
 }
