52c70617de84cfc4dbbf2de7c16a20eef0da3f2b
Daniel R. Reynolds <dreynolds@umbc.edu>
2025-10-07 09:51:33 -0400

    Updated adr_1D driver to support fixed step mode (use namelist input with h .ne. 0)

--- adr_1D/pirock/dr_adr_1D.f
+++ adr_1D/pirock/dr_adr_1D.f
@@ -1,12 +1,12 @@
 c * * * * * * * * * * * * * * * * * * * * * * * * *
 c    Driver for PIROCK
 c * * * * * * * * * * * * * * * * * * * * * * * * *
-c ----- to integrate with pirock.f ----- 
-      include 'pirock.f' 
-      include 'decsol.f' 
-c      include 'namelist_read.txt' 
+c ----- to integrate with pirock.f -----
+      include 'pirock.f'
+      include 'decsol.f'
+c      include 'namelist_read.txt'
 c --- Problem driver and dimension parameters
-	include 'pb_adr_1D.f' 
+	include 'pb_adr_1D.f'
       program main
       parameter(nsd=512,npdes=3,neqn=nsd*npdes)
 c ----------------------------------------------------
@@ -15,13 +15,14 @@ c --- common parameters for the problem -----
       common/trans/alf,amult,ns,nssq,nsnsm1,nsm1sq,eps,atol,rtol,
      &    brussa,brussb,uxadv,vxadv,wxadv,uyadv,vyadv,wyadv,imeth,iwork20,iwork21
       external fd,fd2,fa,fr,fw
-c ----- to integrate with pirock.f 
-      dimension y(neqn),work(15*neqn),frjac(neqn*npdes) 
+c ----- to integrate with pirock.f
+      dimension y(neqn),work(15*neqn),frjac(neqn*npdes)
       integer iwork(25),idid,ijac(neqn)
+      logical fixedstep
 c ----- required tolerance -----
 c --- namelist definition
       namelist /list1/ alf,amult,uxadv,uyadv,vxadv,vyadv,wxadv,wyadv,
-     &                 brussa,brussb,eps,atol,rtol,iwork20,iwork21
+     &                 brussa,brussb,eps,atol,rtol,h,iwork20,iwork21
 
 c --- read input from namelist file (if it exists) ---
       open(10, file='namelist_read.txt', status='old', err=100)
@@ -32,18 +33,24 @@ c --- read input from namelist file (if it exists) ---
   100 continue
       write(6,*) 'Could not open namelist file'
 c ----- initial step size -----
-  110			h=1.d-3
-   			write(6,*) 'initial step size h=',h
+  110			if (h .le. 0.d0) then
+                    fixedstep=.false.
+                    h=1.d-3
+                    write(6,*) 'Initial step size h=',h
+                  else
+                    fixedstep=.true.
+                    write(6,*) 'Fixed step size h=',h
+                  end if
 c                  write(6,*) 'advection driver:', uxadv,vxadv,wxadv
 c                  write(6,*) 'diffusion driver:', alf
 c                  write(6,*) 'reaction driver:', brussa,brussb,eps
 c --------------- multiplying by 1.d0 because of tests that are run from python script
-c --------------- because Python can't take in values with '.d'                   
+c --------------- because Python can't take in values with '.d'
                   atol=atol*1.d0
                   rtol=rtol*1.d0
 
 c--------------------------------------------------------
-c     Initialize iwork: 
+c     Initialize iwork:
 c      iwork(1)=1  RHODIFF returns an upper bound for the spectral radius.
 c      iwork(2)=1  The Jacobian of diffusion is constant (RHODIFF is called once).
 c      iwork(3)=0  Return and solution at tend.
@@ -68,7 +75,11 @@ c--------------------------------------------------------
 c			iwork(19)=2
 c			iwork(23)=0
 c			iwork(24)=0
-			iwork(19)=2
+                  if (fixedstep) then
+                    iwork(19)=0
+                  else
+                    iwork(19)=2
+                  end if
 			iwork(23)=0
 			iwork(24)=0
 c
@@ -78,20 +89,20 @@ c			iwork(22)=0
                   iwork(20)=iwork20
 			iwork(21)=iwork21
 			iwork(22)=0
-	
+
 c iwork for stats
       do i=5,18
 		iwork(i)=0
 	end do
-			
+
       call init(nsd,t,tend,y)
 
 c ----- integration -----
-			write (6,*) 'tol',atol 
+			write (6,*) 'tol',atol
 			CALL CPU_TIME(time0)
-c ----- to integrate with rock2.f   
+c ----- to integrate with rock2.f
       call pirock(neqn,npdes,t,tend,h,y,fd,fd2,fa,fr,fw,atol,rtol,
-     &           frjac,ijac,work,iwork,idid)  
+     &           frjac,ijac,work,iwork,idid)
       CALL CPU_TIME(time1)
 			write (6,*) 'CPU time',time1-time0
 c ----- print statistics -----
@@ -106,18 +117,18 @@ c ----- print statistics -----
       write (6,91) iwork(5),iwork(16),iwork(6),
      &   iwork(7),iwork(8),iwork(13)
  91   format(' Number of f evaluations=',i7,' fA evaluations=',i7,
-     &      ' steps=',i7,' accpt=',i7,' rejct=',i7,' max iter',i4) 
-		 
+     &      ' steps=',i7,' accpt=',i7,' rejct=',i7,' max iter',i4)
+
 		  write (6,*) 'Number of reaction VF',
      &   iwork(17),(iwork(17)*npdes)/neqn
 		  write (6,*) 'Number of reaction Jacobian',
      &   iwork(18),(iwork(18)*npdes)/neqn
-		  
+
 			call solout(neqn,t,tend,y,work)
 
 c--------------------------------------------------------
 c     End of main program
 c--------------------------------------------------------
-      end      
+      end
 
 
